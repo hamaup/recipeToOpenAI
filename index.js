ac825f9bc5dc1ec044c999bf86c693bb6938bf51
hamaup <32893785+hamaup@users.noreply.github.com>
2023-06-20 18:54:47 +0900

    Fixed for Vercel

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const app = require('express')();
 const { v4 } = require('uuid');
+const cors = require('cors');
+
+app.use(cors());
 
 app.get('/api', (req, res) => {
   const path = `/api/item/${v4()}`;
