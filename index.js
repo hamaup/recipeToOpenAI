5ce5212d63c77a7249d47d511955fd4c65a3007e
hamaup <32893785+hamaup@users.noreply.github.com>
2023-06-20 20:09:05 +0900

    Fixed for Vercel

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const cors = require('cors');
 
 app.use(cors());
 
-app.get('/api', (req, res) => {
+app.post('/api', (req, res) => {
   const path = `/api/item/${v4()}`;
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
 
-app.get('/api/item/:slug', (req, res) => {
+app.post('/api/item/:slug', (req, res) => {
   const { slug } = req.params;
   res.end(`Item: ${slug}`);
 });
